9796c05ea7bcde9771d87187dda91f7dffb88377
Vasil Yanchevski <vianchevski@gmail.com>
2023-05-03 22:23:26 +0300

    checked for selected option test

---
+++ src/test/java/CheckSelectedOption.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CheckSelectedOption {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("dropdown")).click();
+
+        WebElement option1 = driver.findElement(By.xpath("//*[@id=\"dropdown\"]/option[2]"));
+        WebElement option2 = driver.findElement(By.xpath("//*[@id=\"dropdown\"]/option[3]"));
+
+
+
+        option1.click();
+
+        if (option1.isSelected()) {
+            System.out.println("Option one is selected");
+        } else if (option2.isSelected()) {
+            System.out.println("Option two is selected");
+        }
+
+        driver.quit();
+    }
+
+}
